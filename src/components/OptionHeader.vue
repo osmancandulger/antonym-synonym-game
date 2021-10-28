9b0b6a5fddff25055fde52fd51ecddf9d6357216
Osman Dülger <osmancandulger@gmail.com>
2021-10-28 14:18:42 +0300

    Fixed: Page title

--- src/components/OptionHeader.vue
+++ src/components/OptionHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <h1>Synonym Game</h1>
+    <h1>Synonym &amp; Meaning Game</h1>
     <!-- <label class="label" for="select">Please select a person: </label>
     <select
       v-model="selectedVoice"
