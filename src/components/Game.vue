0ca57877cc8906dc727afbc3884f01ad82b0a8d5
Osman Dülger <osmancandulger@gmail.com>
2021-10-31 19:53:08 +0300

    Fixed: dontenv

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -61,6 +61,7 @@
 <script lang="ts">
 import { Vue } from 'vue-class-component';
 import { Prop, Watch } from 'vue-property-decorator';
+require('dotenv').config({ path: './.env' });
 export default class Game extends Vue {
   @Prop({ required: true }) private selectedVoice: Object | any;
 
--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,3 @@ module.exports = {
   publicPath:
     process.env.NODE_ENV === 'production' ? '/antonym-synonym-game/' : '/',
 };
-require('dotenv').config({ path: './.env' });
