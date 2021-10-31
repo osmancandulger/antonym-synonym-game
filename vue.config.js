52a6061613bda1a43667135d9c18d77d4a23adce
Osman Dülger <osmancandulger@gmail.com>
2021-10-31 19:51:10 +0300

    Fixed: Vue config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 module.exports = {
   publicPath:
     process.env.NODE_ENV === 'production' ? '/antonym-synonym-game/' : '/',
+  env: require('dotenv').config({ path: './.env' }),
 };
-process.env.NODE_ENV === 'production'
-  ? require('dotenv').config({ path: './.env' })
-  : '';
